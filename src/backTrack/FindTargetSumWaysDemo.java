71c1d758cfab7ebc3d43e3a5f4d2def147c766e3
BeauteousJade <pby2196995023@163.com>
2022-10-07 17:16:50 +0800

    更新代码

--- src/backTrack/FindTargetSumWaysDemo.java
+++ src/backTrack/FindTargetSumWaysDemo.java
@@ -39,6 +39,10 @@ public class FindTargetSumWaysDemo {
         if (diff < 0 || diff % 2 != 0) {
             return 0;
         }
+        //记数组的元素和为 sum，添加- 号的元素之和为neg，则其余添加+的元素之和为sum−neg，
+        // 得到的表达式的结果为：
+        // (sum − neg) − neg= sum − 2 * neg = target
+        // 得出结果：neg = (sum - target)  / 2。
         int neg = diff / 2;
         // dp[i][j]:前i个数，和为j的数量。
         int[][] dp = new int[nums.length + 1][neg + 1];
