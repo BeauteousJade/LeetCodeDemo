d328dc97dfdefe704ebae01462a804b705a09162
BeauteousJade <pby2196995023@163.com>
2022-12-27 21:59:56 +0800

    增加【(490) 迷宫】bfs写法和注释

--- src/dfs/HasPathDemo.java
+++ src/dfs/HasPathDemo.java
@@ -1,5 +1,8 @@
 package dfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 490. 迷宫
  * <p>
@@ -37,6 +40,7 @@ public class HasPathDemo {
         if (start[0] == destination[0] && start[1] == destination[1]) {
             return true;
         }
+        // 只记录起点，因为某些点可以重复经过。
         visited[start[0]][start[1]] = true;
         int r = start[1] + 1, l = start[1] - 1, u = start[0] - 1, d = start[0] + 1;
         while (r < maze[0].length && maze[start[0]][r] == 0) { //一直向右跑，跑到墙处才能停下，尝试转弯
@@ -62,6 +66,41 @@ public class HasPathDemo {
         }
         if (dfs(maze, new int[]{d - 1, start[1]}, destination, visited)) {
             return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * bfs的写法。
+     * 注意的是queue里面存储的起点。
+     */
+    public boolean hasPathV2(int[][] maze, int[] start, int[] destination) {
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        int[][] dirs = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(start);
+        // 只记录起点，因为某些点可以重复经过。
+        visited[start[0]][start[1]] = true;
+        while (!queue.isEmpty()) {
+            int[] s = queue.remove();
+            if (s[0] == destination[0] && s[1] == destination[1]) {
+                return true;
+            }
+            for (int[] dir : dirs) {
+                int x = s[0] + dir[0];
+                int y = s[1] + dir[1];
+                // 朝一个方向走，直到走到头。
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                }
+                if (!visited[x - dir[0]][y - dir[1]]) {
+                    // 然后把新的起点，放到queue里面，便于下次搜索。
+                    queue.add(new int[]{x - dir[0], y - dir[1]});
+                    visited[x - dir[0]][y - dir[1]] = true;
+                }
+            }
         }
         return false;
     }
