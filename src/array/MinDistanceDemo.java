3261d6584eb9dc831b700e9a5d866b8a96fafa1c
BeauteousJade <pby2196995023@163.com>
2023-01-07 00:00:19 +0800

    增加【(573) 松鼠模拟】的注释

--- src/array/MinDistanceDemo.java
+++ src/array/MinDistanceDemo.java
@@ -20,10 +20,13 @@ public class MinDistanceDemo {
     public int minDistance(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
         int res = Integer.MAX_VALUE;
         int sum = 0;
+        // 首先计算，所有松果到树的距离，* 2是因为取得松果，还要回到树。
         for (int[] nut : nuts) {
             sum += getDistance(nut, tree) * 2;
         }
         for (int[] firstNut : nuts) {
+            // 然后，以每个松果为起点，计算最小距离。
+            // 因为第一个松果不用去树，所以可以直接去，这里需要加上松鼠到第一个松果的距离，并且要减去树到第一个松果的距离(因为只是单行程)
             int distance = sum - getDistance(firstNut, tree) + getDistance(squirrel, firstNut);
             res = Math.min(res, distance);
         }
