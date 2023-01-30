a2d6c5d9c1307fb3e78ef2380991b1c738951bd4
BeauteousJade <pby2196995023@163.com>
2023-01-30 21:49:26 +0800

    (694) 不同岛屿的数量 -- dfs

---
+++ src/dfs/NumDistinctIslandsDemo.java
@@ -0,0 +1,49 @@
+package dfs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 694. 不同岛屿的数量 -- dfs
+ * <p>
+ * https://leetcode.cn/problems/number-of-distinct-islands/
+ */
+public class NumDistinctIslandsDemo {
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 1, 0, 1, 1}, {1, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {1, 1, 0, 1, 1}};
+        NumDistinctIslandsDemo demo = new NumDistinctIslandsDemo();
+        System.out.println(demo.numDistinctIslands(array));
+    }
+
+
+    public int numDistinctIslands(int[][] grid) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    dfs(i, j, 100, grid, stringBuilder);
+                    set.add(stringBuilder.toString());
+                }
+            }
+        }
+        return set.size();
+    }
+
+
+    private void dfs(int i, int j, int dir, int[][] grid, StringBuilder sb) {
+        int n = grid.length;
+        int m = grid[0].length;
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        sb.append(dir);
+        dfs(i + 1, j, 1, grid, sb);
+        dfs(i - 1, j, 2, grid, sb);
+        dfs(i, j + 1, 3, grid, sb);
+        dfs(i, j - 1, 4, grid, sb);
+        sb.append(-dir);
+    }
+}
