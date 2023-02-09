e74b9aa4cb0a4c3ade067a12b08f032589938b38
BeauteousJade <pby2196995023@163.com>
2023-02-09 20:40:14 +0800

    更新【419. 甲板上的战舰】的代码

--- src/dfs/CountBattleshipsDemo.java
+++ src/dfs/CountBattleshipsDemo.java
@@ -19,7 +19,8 @@ public class CountBattleshipsDemo {
         boolean[][] rec = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (!rec[i][j] && board[i][j] == 'X' && help(board, rec, i, j)) {
+                if (!rec[i][j] && board[i][j] == 'X') {
+                    help(board, rec, i, j);
                     count++;
                 }
                 rec[i][j] = true;
@@ -28,20 +29,16 @@ public class CountBattleshipsDemo {
         return count;
     }
 
-    private boolean help(char[][] board, boolean[][] rec, int i, int j) {
-        if (i >= board.length || j >= board[i].length) {
-            return true;
+    private void help(char[][] board, boolean[][] rec, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length || board[i][j] != 'X' || rec[i][j]) {
+            return;
         }
-        if (board[i][j] == 'X') {
-            rec[i][j] = true;
-            if (i + 1 < board.length && board[i + 1][j] == 'X') {
-                return help(board, rec, i + 1, j);
-            }
-            if (j + 1 < board[i].length && board[i][j + 1] == 'X') {
-                return help(board, rec, i, j + 1);
-            }
-            return true;
+        rec[i][j] = true;
+        if (i + 1 < board.length && board[i + 1][j] == 'X') {
+            help(board, rec, i + 1, j);
         }
-        return false;
+        if (j + 1 < board[i].length && board[i][j + 1] == 'X') {
+            help(board, rec, i, j + 1);
+        }
     }
 }
