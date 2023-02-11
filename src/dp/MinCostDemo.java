76d8c1b66328b9b1fad683145c0a661ef17d0cc6
BeauteousJade <pby2196995023@163.com>
2023-02-11 18:09:44 +0800

    增加【256. 粉刷房子】的解法

--- src/dp/MinCostDemo.java
+++ src/dp/MinCostDemo.java
@@ -1,5 +1,10 @@
 package dp;
 
+/**
+ * 256. 粉刷房子
+ * <p>
+ * https://leetcode.cn/problems/paint-house/
+ */
 public class MinCostDemo {
 
 
@@ -26,5 +31,23 @@ public class MinCostDemo {
         }
 
         return Math.min(a, Math.min(b, c));
+    }
+
+    public int minCostV2(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+        // dp[房子下标][房子颜色]：最小花费。
+        int[][] dp = new int[costs.length][3];
+        dp[0][0] = costs[0][0];
+        dp[0][1] = costs[0][1];
+        dp[0][2] = costs[0][2];
+        for (int i = 1; i < costs.length; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+        }
+
+        return Math.min(dp[dp.length - 1][0], Math.min(dp[dp.length - 1][1], dp[dp.length - 1][2]));
     }
 }
