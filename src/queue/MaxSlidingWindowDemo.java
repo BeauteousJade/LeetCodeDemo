1d5267d7fa97c889a4a03fba0127ac02c56b21c0
BeauteousJade <pby2196995023@163.com>
2023-02-11 15:31:16 +0800

    删除【239. 滑动窗口最大值】没必要的代码，使其更加符合规范

--- src/queue/MaxSlidingWindowDemo.java
+++ src/queue/MaxSlidingWindowDemo.java
@@ -1,7 +1,6 @@
-package topk;
+package queue;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -21,13 +20,8 @@ public class MaxSlidingWindowDemo {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
         int[] res = new int[nums.length - k + 1];
-        // 按照值从小到排序，按照index从大到小排序(降序、升序都一样)。
-        Queue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
-            }
-        });
+        // 按照值从大到小排序。
+        Queue<int[]> queue = new PriorityQueue<>((o1, o2) -> o2[0] - o1[0]);
         for (int i = 0; i < k; i++) {
             queue.offer(new int[]{nums[i], i});
         }
