e30ca625cf8a6532cec9f2b847b200d9d496ed13
BeauteousJade <pby2196995023@163.com>
2023-02-11 22:07:00 +0800

    简化【165. 比较版本号】的代码

--- src/other/CompareVersionDemo.java
+++ src/other/CompareVersionDemo.java
@@ -15,31 +15,16 @@ public class CompareVersionDemo {
     public int compareVersion(String version1, String version2) {
         String[] strings1 = version1.split("\\.");
         String[] strings2 = version2.split("\\.");
-        int count = Math.min(strings1.length, strings2.length);
-        for (int i = 0; i < count; i++) {
-            int value1 = Integer.parseInt(strings1[i]);
-            int value2 = Integer.parseInt(strings2[i]);
-            if (value1 > value2) {
-                return 1;
-            } else if (value1 < value2) {
+        int index1 = 0;
+        int index2 = 0;
+        while (index1 < strings1.length || index2 < strings2.length) {
+            int a = index1 < strings1.length ? Integer.parseInt(strings1[index1++]) : 0;
+            int b = index2 < strings2.length ? Integer.parseInt(strings2[index2++]) : 0;
+            if (a < b) {
                 return -1;
-            }
-        }
-
-        int index = count;
-        while (index < strings1.length) {
-            int value1 = Integer.parseInt(strings1[index]);
-            if (value1 != 0) {
+            } else if (a > b) {
                 return 1;
             }
-            index++;
-        }
-        while (index < strings2.length) {
-            int value2 = Integer.parseInt(strings2[index]);
-            if (value2 != 0) {
-                return -1;
-            }
-            index++;
         }
         return 0;
     }
