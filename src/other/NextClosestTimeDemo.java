4a6e5a91da1e5ffb7c7f8abc5aabe7d27de9f7c0
BeauteousJade <pby2196995023@163.com>
2023-02-11 22:09:42 +0800

    增加【681. 最近时刻】的注释

--- src/other/NextClosestTimeDemo.java
+++ src/other/NextClosestTimeDemo.java
@@ -18,6 +18,7 @@ public class NextClosestTimeDemo {
         Set<String> hh = new HashSet<>();
         Set<String> mm = new HashSet<>();
         Set<Character> set = new HashSet<>();
+        // 1. 统计出现的数字。
         for (int i = 0; i < time.length(); i++) {
             char c = time.charAt(i);
             if (c != ':') {
@@ -25,6 +26,7 @@ public class NextClosestTimeDemo {
             }
         }
 
+        // 2. 分别构造小时和分钟，并且分别记录再hh和mm集合里面。
         for (char c1 : set) {
             for (char c2 : set) {
                 String str = c1 + "" + c2;
@@ -37,6 +39,7 @@ public class NextClosestTimeDemo {
             }
         }
 
+        // 3.构造时间。
         List<String> memo = new ArrayList<>();
         for (String h : hh) {
             for (String m : mm) {
@@ -44,7 +47,9 @@ public class NextClosestTimeDemo {
             }
         }
 
+        // 4. 排序。
         Collections.sort(memo);
+        // 5. 获取下一个时间，所以没有下一个，就取第一个.
         int index = memo.indexOf(time);
         if (index == memo.size() - 1) {
             return memo.get(0);
