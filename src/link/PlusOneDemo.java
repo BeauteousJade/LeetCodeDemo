98051671642b8cd48c9afa10cd3e633cf1ffca60
BeauteousJade <pby2196995023@163.com>
2023-02-26 20:56:40 +0800

    修改【369. 给单链表加一】的错误代码

--- src/link/PlusOneDemo.java
+++ src/link/PlusOneDemo.java
@@ -15,6 +15,7 @@ public class PlusOneDemo {
         ListNode preHead = new ListNode(0);
         preHead.next = head;
         ListNode notNine = preHead;
+        // 1. 找到最后一个不是9的节点。
         while (head != null) {
             if (head.val != 9) {
                 notNine = head;
@@ -23,10 +24,12 @@ public class PlusOneDemo {
         }
         notNine.val++;
         notNine = notNine.next;
+        // 2. 把最后一个不是9节点后面的节点都设置为0.
         while (notNine != null) {
             notNine.val = 0;
             notNine = notNine.next;
         }
-        return notNine.val == 0 ? notNine.next : notNine;
+        // 3. 判断最后一个非9节点是否第一个节点，如果是，则需要进位。
+        return preHead.val == 0 ? preHead.next : preHead;
     }
 }
