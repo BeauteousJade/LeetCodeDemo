931a26a8d0490757dd6990d01e7c1449f89e17f9
BeauteousJade <pby2196995023@163.com>
2023-02-27 21:27:22 +0800

    增加【294. 翻转游戏 II】的注释

--- src/memory/CanWinDemo.java
+++ src/memory/CanWinDemo.java
@@ -30,14 +30,22 @@ public class CanWinDemo {
         return dfs(mask);
     }
 
+    /**
+     * 返回为true，表示当前选手会赢。
+     * 第一次是己方，第二次是对方。
+     */
     boolean dfs(long mask) {
+        // 记忆化搜索。已经有结果的存起来，避免重复计算。
         if (memo.containsKey(mask)) {
             return memo.get(mask);
         }
         for (int i = 1; i < n; ++i) {
+            // 如果连续的两个不是反转为0,则放弃本次。
             if ((mask & (1L << i)) == 0 || (mask & (1L << i - 1)) == 0) {
                 continue;
             }
+            // 此时返回为true，表示是对方会赢。
+            // 因为己方已经操作，操作：将第i位和第i - 1位反转位0.
             if (dfs(mask ^ (1L << i ^ (1L << i - 1)))) {
                 continue;// 对方必胜则不选这条路
             }
