ab3f74d765c804ca7df45ff3a2f959da30e63a76
BeauteousJade <pby2196995023@163.com>
2023-03-01 22:20:15 +0800

    增加【663. 均匀树划分】的注释

--- src/tree/CheckEqualTreeDemo.java
+++ src/tree/CheckEqualTreeDemo.java
@@ -17,10 +17,11 @@ public class CheckEqualTreeDemo {
 
     public boolean checkEqualTree(TreeNode root) {
         int total = sum(root);
-        // 这里弹出，避免左右子树加起来正好等于root.
+        // 这里弹出的总和，综合不参与计算。避免左右子树加起来正好等于root.
         stack.pop();
         if (total % 2 == 0) {
             for (int sum : stack) {
+                // 这里判断的是总和的一半。如果有一个子树和等于总和一般，那么另一边的子树也是总和的一半，这个子树带Root。
                 if (sum == total / 2) {
                     return true;
                 }
