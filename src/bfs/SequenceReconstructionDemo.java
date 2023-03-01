acc5f0bd57a8fedb31d55b92ca631bd647f6c39e
BeauteousJade <pby2196995023@163.com>
2023-03-01 21:11:30 +0800

    增加【444. 序列重建】的写法，并且将文件移动到正确目录下

--- src/bfs/SequenceReconstructionDemo.java
+++ src/bfs/SequenceReconstructionDemo.java
@@ -1,4 +1,4 @@
-package topologicalSort;
+package bfs;
 
 import java.util.*;
 
@@ -20,12 +20,14 @@ public class SequenceReconstructionDemo {
             }
             list.add(res);
         }
-        System.out.println(demo.sequenceReconstruction(new int[]{1, 2, 3}, list));
+        System.out.println(demo.sequenceReconstructionV2(new int[]{1, 2, 3}, list));
     }
 
     public boolean sequenceReconstruction(int[] nums, List<List<Integer>> sequences) {
         int n = nums.length;
+        // 记录每个num 拥有上一个数字的个数。
         int[] counts = new int[n + 1];
+        // 记录每个节点的下一个数字。
         Set<Integer>[] sets = new Set[n + 1];
         for (int i = 1; i <= n; i++) {
             sets[i] = new HashSet<Integer>();
@@ -41,6 +43,7 @@ public class SequenceReconstructionDemo {
         }
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
+            // 没有前置数字，表示可以直接入队。
             if (counts[i] == 0) {
                 queue.offer(i);
             }
@@ -57,6 +60,54 @@ public class SequenceReconstructionDemo {
                 counts[i]--;
                 if (counts[i] == 0) {
                     queue.offer(i);
+                }
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 用普通的Map来维护状态--超时。
+     */
+    public boolean sequenceReconstructionV2(int[] nums, List<List<Integer>> sequences) {
+        int n = nums.length;
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (List<Integer> sequence : sequences) {
+            int size = sequence.size();
+            for (int i = 1; i < size; i++) {
+                Integer prev = sequence.get(i - 1);
+                Integer current = sequence.get(i);
+                Set<Integer> set = map.computeIfAbsent(current, integer -> new HashSet<>());
+                set.add(prev);
+
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            // 没有前置数字，表示可以直接入队。
+            if (!map.containsKey(i)) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            // 如果当前queue的size大于1，证明了两点：
+            // 1. 序列下一个数字不是唯一的。
+            // 2. 可能存在有某些数组没有出现在sequences里面,证明nums不是最短序列。
+            if (queue.size() > 1) {
+                return false;
+            }
+            Integer poll = queue.poll();
+            Set<Integer> keySet = new HashSet<>(map.keySet());
+            for (Integer key : keySet) {
+                Set<Integer> set = map.get(key);
+                if (set == null) {
+                    continue;
+                }
+                boolean isRemove = set.remove(poll);
+                if (set.isEmpty() && isRemove) {
+                    queue.offer(key);
+                    map.remove(key);
                 }
             }
         }
