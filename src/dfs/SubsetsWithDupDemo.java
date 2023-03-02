5a0129e32381ad85d5a805c792b4bc400dfd11de
BeauteousJade <pby2196995023@163.com>
2023-03-02 20:58:30 +0800

    90. 子集 II -- dfs

---
+++ src/dfs/SubsetsWithDupDemo.java
@@ -0,0 +1,46 @@
+package dfs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 90. 子集 II -- dfs
+ * <p>
+ * https://leetcode.cn/problems/subsets-ii/
+ */
+public class SubsetsWithDupDemo {
+
+    public static void main(String[] args) {
+        SubsetsWithDupDemo demo = new SubsetsWithDupDemo();
+        int[] array = {4, 4, 4, 1, 4};
+        System.out.println(demo.subsetsWithDup(array));
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        dfs(false, 0, 0, nums, list);
+        return list;
+    }
+
+    private void dfs(boolean doPre, int flag, int index, int[] nums, List<List<Integer>> list) {
+        if (index == nums.length) {
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < index; i++) {
+                if (((flag >> i) & 1) == 1) {
+                    res.add(nums[i]);
+                }
+            }
+            list.add(res);
+        } else {
+            dfs(false, flag, index + 1, nums, list);
+            // 如果前一个值跟当前值相同，并且没有被选中，那么就忽略本次。
+            // 因为这个值再第一次出现的时候就被选中了，不必再次选择。
+            if (!doPre && index > 0 && nums[index - 1] == nums[index]) {
+                return;
+            }
+            dfs(true, flag | 1 << index, index + 1, nums, list);
+        }
+    }
+}
