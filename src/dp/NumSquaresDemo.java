5344a28f646b1ed365bd71a665f5d1469ec861fc
BeauteousJade <pby2196995023@163.com>
2023-03-07 22:00:11 +0800

    增加【279. 完全平方数】的注释

--- src/dp/NumSquaresDemo.java
+++ src/dp/NumSquaresDemo.java
@@ -17,6 +17,7 @@ public class NumSquaresDemo {
         for (int i = 1; i <= n; i++) {
             dp[i] = Integer.MAX_VALUE;
             for (int j = 1; j * j <= i; j++) {
+                // dp[i - j * j]:以j拆分当前数.
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
