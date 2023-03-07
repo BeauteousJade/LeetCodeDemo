63960ffaa62f4a51960b7dd2131e96041594a74a
BeauteousJade <pby2196995023@163.com>
2023-03-07 22:01:04 +0800

    增加【198. 打家劫舍】的注释

--- src/dp/RobDemo.java
+++ src/dp/RobDemo.java
@@ -24,6 +24,8 @@ public class RobDemo {
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
         for (int i = 2; i < nums.length; i++) {
+            // dp[i - 1]:当前不偷。
+            // dp[i - 2] + nums[i]:当前偷，那么上一个就不偷。
             dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         }
         return dp[nums.length - 1];
