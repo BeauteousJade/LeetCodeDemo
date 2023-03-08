d0336b291e16220541b9810200a988e6a1f76bb9
BeauteousJade <pby2196995023@163.com>
2023-03-08 21:07:38 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字 -- 二分搜索

---
+++ src/binarySearch/MissingNumberDemo.java
@@ -0,0 +1,32 @@
+package binarySearch;
+
+/**
+ * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ * <p>
+ * https://leetcode.cn/problems/que-shi-de-shu-zi-lcof/
+ */
+public class MissingNumberDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 1.先看是否去等于，如果等于就两个都变，结果优先选择left。
+     * 2.如果不能等于就一个变，最终结果选择变得那个。
+     */
+    public int missingNumber(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            // 如果当前值等于 mid，表示丢失的数字在右侧。
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
