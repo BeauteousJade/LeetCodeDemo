afaa7e325d6b65e12ba5c3ad101e774ccf6562ce
BeauteousJade <pby2196995023@163.com>
2023-03-08 22:02:42 +0800

    剑指 Offer 62. 圆圈中最后剩下的数字 -- dp & 约瑟夫环问题

---
+++ src/dp/LastRemainingDemo.java
@@ -0,0 +1,39 @@
+package dp;
+
+/**
+ * 剑指 Offer 62. 圆圈中最后剩下的数字
+ * <p>
+ * 约瑟夫环问题
+ * <p>
+ * https://leetcode.cn/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+ */
+public class LastRemainingDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * https://leetcode.cn/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/solution/jian-zhi-offer-62-yuan-quan-zhong-zui-ho-dcow/
+     */
+    public int lastRemaining(int n, int m) {
+        // dp[数字的数量]：在指定数量的数字下，最后的结果。
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + m) % i;
+        }
+        return dp[n];
+    }
+
+    /**
+     * dp 压缩版。
+     */
+    public int lastRemainingV2(int n, int m) {
+        int x = 0;
+        for (int i = 2; i <= n; i++) {
+            x = (x + m) % i;
+        }
+        return x;
+    }
+}
