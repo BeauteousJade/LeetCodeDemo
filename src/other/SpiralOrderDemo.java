7bd447fa3a022d5c7f182cadf5b874c82face4e9
BeauteousJade <pby2196995023@163.com>
2023-03-09 21:32:29 +0800

    剑指 Offer 29. 顺时针打印矩阵

---
+++ src/other/SpiralOrderDemo.java
@@ -0,0 +1,50 @@
+package other;
+
+/**
+ * 剑指 Offer 29. 顺时针打印矩阵
+ * <p>
+ * https://leetcode.cn/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+ */
+public class SpiralOrderDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] spiralOrder(int[][] matrix) {
+        if (matrix.length == 0) return new int[0];
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int x = 0;
+        int[] res = new int[(right + 1) * (bottom + 1)];
+        while (true) {
+            for (int i = left; i <= right; i++) {
+                res[x++] = matrix[top][i]; // left to right.
+            }
+            if (++top > bottom) { // 换下一行。
+                break;
+            }
+            for (int i = top; i <= bottom; i++) {
+                res[x++] = matrix[i][right]; // top to bottom.
+            }
+            if (left > --right) { // 换到左边的列
+                break;
+            }
+            for (int i = right; i >= left; i--) {
+                res[x++] = matrix[bottom][i]; // right to left.
+            }
+            if (top > --bottom) { // 换到上一行
+                break;
+            }
+            for (int i = bottom; i >= top; i--) {
+                res[x++] = matrix[i][left]; // bottom to top.
+            }
+            if (++left > right) { // 换到右边的列。
+                break;
+            }
+        }
+        return res;
+    }
+}
