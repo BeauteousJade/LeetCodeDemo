af0f66e819c532fdd29f9274d41d3593bee4e97a
BeauteousJade <pby2196995023@163.com>
2023-03-12 19:33:16 +0800

    143. 重排链表 -- 链表

--- src/link/ReorderListDemo.java
+++ src/link/ReorderListDemo.java
@@ -11,58 +11,24 @@ public class ReorderListDemo {
 
     }
 
-    public void reorderList(ListNode head) {
+    private ListNode firstNode;
+
+    public boolean isPalindrome(ListNode head) {
+        firstNode = head;
+        return dfs(head);
+    }
+
+    private boolean dfs(ListNode head) {
         if (head == null) {
-            return;
+            return true;
         }
-        // 1. 先找到中间节点。
-        ListNode middleNode = findMiddleNode(head);
-        ListNode l1 = head;
-        ListNode l2 = middleNode.next;
-        middleNode.next = null;
-        // 2. 将右侧部分的节点给反转了，记住上面将middle.next设置为空了，不然会形成环。
-        l2 = reverseListNode(l2);
-        // 3. 合并。
-        mergeListNode(l1, l2);
-    }
-
-
-    private ListNode findMiddleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (!dfs(head.next)) {
+            return false;
         }
-        return slow;
-    }
-
-    private ListNode reverseListNode(ListNode head) {
-        ListNode preNode = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = preNode;
-            preNode = head;
-            head = next;
+        if (firstNode.val != head.val) {
+            return false;
         }
-        return preNode;
-    }
-
-    private void mergeListNode(ListNode l1, ListNode l2) {
-        ListNode preHead = new ListNode(0);
-        ListNode node = preHead;
-        while (l1 != null || l2 != null) {
-            if (l1 != null) {
-                node.next = l1;
-                l1 = l1.next;
-                node = node.next;
-            }
-            if (l2 != null) {
-                node.next = l2;
-                l2 = l2.next;
-                node = node.next;
-            }
-        }
-        preHead.next = null;
+        firstNode = firstNode.next;
+        return true;
     }
 }
