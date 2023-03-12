7ed8d05a0f3822568dac3d0844d1d798873beffd
BeauteousJade <pby2196995023@163.com>
2023-03-12 20:56:55 +0800

    剑指 Offer II 031. 最近最少使用缓存

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -1,17 +1,59 @@
-import java.util.HashMap;
-import java.util.Map;
+import link.ListNode;
 
 public class TestDemo {
 
     public static void main(String[] args) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(1, 1);
-        map.put(5, 1);
-        map.put(4, 1);
-        map.put(2, 1);
-        map.put(100, 1);
+        TestDemo demo = new TestDemo();
 
-        System.out.println(map.keySet());
     }
 
+    public Node insert(Node head, int insertVal) {
+        if (head == null) {
+            Node listNode = new Node(insertVal);
+            listNode.next = listNode;
+            return listNode;
+        }
+        if (head.next == null) {
+            Node listNode = new Node(insertVal);
+            head.next = listNode;
+            listNode.next = head;
+            return head;
+        }
+
+        Node current = head;
+        Node next = head.next;
+        while (next != head) {
+            if (current.val <= insertVal && next.val >= insertVal) {
+                break;
+            }
+            // 环
+            if (current.val > next.val) {
+                if (insertVal >= current.val || insertVal <= next.val) {
+                    break;
+                }
+            }
+            current = current.next;
+            next = next.next;
+        }
+        current.next = new Node(insertVal);
+        current.next.next = next;
+        return head;
+    }
+
+    class Node {
+        public int val;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _next) {
+            val = _val;
+            next = _next;
+        }
+    }
 }
