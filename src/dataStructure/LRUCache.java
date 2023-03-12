801f0f4ae3e67ba8b2709840211a607c267ccb86
BeauteousJade <pby2196995023@163.com>
2023-03-12 21:03:01 +0800

    剑指 Offer II 031. 最近最少使用缓存

---
+++ src/dataStructure/LRUCache.java
@@ -0,0 +1,99 @@
+package dataStructure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 剑指 Offer II 031. 最近最少使用缓存
+ */
+public class LRUCache {
+
+    private static final class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        public Node() {
+        }
+
+        public Node(int _key, int _value) {
+            key = _key;
+            value = _value;
+        }
+    }
+
+    private final Map<Integer, Node> cache = new HashMap<Integer, Node>();
+    private int size;
+    private final int capacity;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        // 使用伪头部和伪尾部节点
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        Node node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        // 如果 key 存在，先通过哈希表定位，再移到头部
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        Node node = cache.get(key);
+        if (node == null) {
+            // 如果 key 不存在，创建一个新的节点
+            Node newNode = new Node(key, value);
+            // 添加进哈希表
+            cache.put(key, newNode);
+            // 添加至双向链表的头部
+            addToHead(newNode);
+            ++size;
+            if (size > capacity) {
+                // 如果超出容量，删除双向链表的尾部节点
+                Node tail = removeTail();
+                // 删除哈希表中对应的项
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    private void addToHead(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private Node removeTail() {
+        Node res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+}
