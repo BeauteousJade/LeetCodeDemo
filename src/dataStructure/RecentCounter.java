f654af97737497a643138cd0eced4de370910c3e
BeauteousJade <pby2196995023@163.com>
2023-03-13 21:28:12 +0800

    933. 最近的请求次数

---
+++ src/dataStructure/RecentCounter.java
@@ -0,0 +1,26 @@
+package dataStructure;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 933. 最近的请求次数
+ * <p>
+ * https://leetcode.cn/problems/number-of-recent-calls/
+ */
+class RecentCounter {
+
+    private Queue<Integer> queue = new LinkedList<>();
+
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+        while (!queue.isEmpty() && queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
