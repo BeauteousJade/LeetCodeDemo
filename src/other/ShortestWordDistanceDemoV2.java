2315425a3814064dee986bf33035b777742c7c68
BeauteousJade <pby2196995023@163.com>
2023-03-13 21:20:33 +0800

    增加【245. 最短单词距离 III】的简单写法。

--- src/other/ShortestWordDistanceDemoV2.java
+++ src/other/ShortestWordDistanceDemoV2.java
@@ -28,4 +28,35 @@ public class ShortestWordDistanceDemoV2 {
         }
         return ans;
     }
+
+    public int shortestWordDistanceV2(String[] wordsDict, String word1, String word2) {
+        int length = wordsDict.length;
+        int ans = length;
+        if (word1.equals(word2)) {
+            int prev = -1;
+            for (int i = 0; i < length; i++) {
+                String word = wordsDict[i];
+                if (word.equals(word1)) {
+                    if (prev >= 0) {
+                        ans = Math.min(ans, i - prev);
+                    }
+                    prev = i;
+                }
+            }
+        } else {
+            int index1 = -1, index2 = -1;
+            for (int i = 0; i < length; i++) {
+                String word = wordsDict[i];
+                if (word.equals(word1)) {
+                    index1 = i;
+                } else if (word.equals(word2)) {
+                    index2 = i;
+                }
+                if (index1 >= 0 && index2 >= 0) {
+                    ans = Math.min(ans, Math.abs(index1 - index2));
+                }
+            }
+        }
+        return ans;
+    }
 }
