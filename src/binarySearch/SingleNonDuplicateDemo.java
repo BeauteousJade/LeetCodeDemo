9bcb245ddaedda7c779aeb8072a3eb12fe11f3b3
BeauteousJade <pby2196995023@163.com>
2023-03-22 21:13:31 +0800

    增加【540. 有序数组中的单一元素 】的注释

--- src/binarySearch/SingleNonDuplicateDemo.java
+++ src/binarySearch/SingleNonDuplicateDemo.java
@@ -17,12 +17,17 @@ public class SingleNonDuplicateDemo {
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (mid < nums.length - 1 && nums[mid] == nums[mid + 1]) {
+                // 如果当前mid 等于 偶数，且nums[mid] == nums[mid + 1]，表示left 之前的都是出现两次的数。
+                // 所以结果应该在 right 上。
                 if (mid % 2 == 0) {
                     left = mid + 2;
                 } else {
+                    // 反之则在left 上。
                     right = mid - 1;
                 }
             } else if (mid > 0 && nums[mid] == nums[mid - 1]) {
+                // 如果当前的mid 等于 偶数，且nums[mid] == nums [mid - 1]，left 上出现了一个单数。
+                // 此时right = mid - 2。
                 if (mid % 2 == 0) {
                     right = mid - 2;
                 } else {
