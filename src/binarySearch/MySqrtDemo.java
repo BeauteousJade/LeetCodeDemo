07d61340f761ff80e18eca15b8da90922ee08ed6
BeauteousJade <pby2196995023@163.com>
2023-03-23 20:40:09 +0800

    69. x 的平方根 -- 二分

---
+++ src/binarySearch/MySqrtDemo.java
@@ -0,0 +1,27 @@
+package binarySearch;
+
+/**
+ * 69. x 的平方根
+ * <p>
+ * https://leetcode.cn/problems/sqrtx/
+ */
+public class MySqrtDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left - 1;
+    }
+}
