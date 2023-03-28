3141797fe464125ce83abf8c84823c653aac7acf
BeauteousJade <pby2196995023@163.com>
2023-03-28 21:21:40 +0800

    简化【90. 子集 II -- dfs】的代码

--- src/dfs/SubsetsWithDupDemo.java
+++ src/dfs/SubsetsWithDupDemo.java
@@ -20,11 +20,11 @@ public class SubsetsWithDupDemo {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
-        dfs(false, 0, 0, nums, list);
+        dfs(0, 0, nums, list);
         return list;
     }
 
-    private void dfs(boolean doPre, int flag, int index, int[] nums, List<List<Integer>> list) {
+    private void dfs(int flag, int index, int[] nums, List<List<Integer>> list) {
         if (index == nums.length) {
             List<Integer> res = new ArrayList<>();
             for (int i = 0; i < index; i++) {
@@ -34,13 +34,13 @@ public class SubsetsWithDupDemo {
             }
             list.add(res);
         } else {
-            dfs(false, flag, index + 1, nums, list);
+            dfs(flag, index + 1, nums, list);
             // 如果前一个值跟当前值相同，并且没有被选中，那么就忽略本次。
             // 因为这个值再第一次出现的时候就被选中了，不必再次选择。
-            if (!doPre && index > 0 && nums[index - 1] == nums[index]) {
+            if (index > 0 && nums[index - 1] == nums[index] && (flag & (1 << (index - 1))) == 0) {
                 return;
             }
-            dfs(true, flag | 1 << index, index + 1, nums, list);
+            dfs(flag | 1 << index, index + 1, nums, list);
         }
     }
 }
