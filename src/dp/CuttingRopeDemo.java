263a35a8d7291f467400100ba796c7fd89c32b0f
BeauteousJade <pby2196995023@163.com>
2023-03-29 21:52:53 +0800

    更新【剑指 Offer 14- I. 剪绳子】的注释

--- src/dp/CuttingRopeDemo.java
+++ src/dp/CuttingRopeDemo.java
@@ -3,7 +3,7 @@ package dp;
 /**
  * 剑指 Offer 14- I. 剪绳子
  * <p>
- * https://leetcode.cn/submissions/detail/410096168/
+ * https://leetcode.cn/problems/jian-sheng-zi-lcof/
  */
 public class CuttingRopeDemo {
 
@@ -17,6 +17,7 @@ public class CuttingRopeDemo {
         dp[2] = 1;
         for (int i = 3; i <= n; i++) {
             for (int j = 1; j < i; j++) {
+                // 1.当前减去j，那么对应的结果可能是 j * dp[i - j](非第一刀)，可能是j * (i - j)(第一刀)
                 dp[i] = Math.max(dp[i], Math.max(j * dp[i - j], j * (i - j)));
             }
         }
