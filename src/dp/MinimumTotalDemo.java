2574bba5d7869dc80d240a4aea800d59a0e8014d
BeauteousJade <pby2196995023@163.com>
2023-04-04 21:02:52 +0800

    120. 三角形最小路径和 -- dp

---
+++ src/dp/MinimumTotalDemo.java
@@ -0,0 +1,35 @@
+package dp;
+
+import java.util.List;
+
+/**
+ * 120. 三角形最小路径和
+ * <p>
+ * https://leetcode.cn/problems/triangle/
+ */
+public class MinimumTotalDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; ++i) {
+            // 第一个只从上一个继承。
+            dp[i][0] = dp[i - 1][0] + triangle.get(i).get(0);
+            for (int j = 1; j < i; ++j) {
+                dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle.get(i).get(j);
+            }
+            // 每行最后一个，只从左上一个继承。
+            dp[i][i] = dp[i - 1][i - 1] + triangle.get(i).get(i);
+        }
+        int minTotal = dp[n - 1][0];
+        for (int i = 1; i < n; ++i) {
+            minTotal = Math.min(minTotal, dp[n - 1][i]);
+        }
+        return minTotal;
+    }
+}
