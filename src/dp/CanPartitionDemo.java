6e231f496634b4f831ce616149d855bba0750915
BeauteousJade <pby2196995023@163.com>
2023-04-04 21:04:57 +0800

    简化【416. 分割等和子集】的代码

--- src/dp/CanPartitionDemo.java
+++ src/dp/CanPartitionDemo.java
@@ -1,5 +1,7 @@
 package dp;
 
+import java.util.Arrays;
+
 /**
  * 416. 分割等和子集
  * <p>
@@ -16,39 +18,27 @@ public class CanPartitionDemo {
      * 0-1背包问题。
      */
     public boolean canPartition(int[] nums) {
-        int total = 0;
-        int maxNum = 0;
-        for (int num : nums) {
-            total += num;
-            maxNum = Math.max(maxNum, num);
-        }
-        if (total % 2 != 0) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 2 != 0) {
             return false;
         }
-        int target = total / 2;
-        if (maxNum > target) {
-            return false;
-        }
+        int target = sum / 2;
+        int m = nums.length;
         // dp[i][j]:第i个数字，和为j,是否有子集符合。
-        boolean[][] dp = new boolean[nums.length][target + 1];
-        for (int i = 0; i < nums.length; i++) {
-            dp[i][0] = true;
-        }
-        dp[0][nums[0]] = true;
+        boolean[][] dp = new boolean[m + 1][target + 1];
+        dp[0][0] = true;
 
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i <= nums.length; i++) {
             for (int j = 1; j <= target; j++) {
-                if (j >= nums[i]) {
-                    // dp[i - 1][j]:不选择当前i,继承i - 1;
+                // dp[i - 1][j]:不选择当前i,继承i - 1;
+                dp[i][j] = dp[i - 1][j];
+                if (j >=nums[i - 1]) {
                     // dp[i - 1][j - nums[i]]:选择当前i;
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
                 }
             }
         }
-        return dp[nums.length - 1][target];
+        return dp[m][target];
     }
-
 
 }
