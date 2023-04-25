cd9df2d49f372435a46949e99a84b0b829c23099
BeauteousJade <pby2196995023@163.com>
2023-04-25 21:37:34 +0800

    354. 俄罗斯套娃信封问题 -- dp

---
+++ src/dp/MaxEnvelopesDemo.java
@@ -0,0 +1,97 @@
+package dp;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 354. 俄罗斯套娃信封问题
+ * <p>
+ * https://leetcode.cn/problems/russian-doll-envelopes/
+ */
+public class MaxEnvelopesDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 普通dp。时间复杂度:O(n * n)
+     */
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    /**
+     * dp & 二分。时间复杂度:O(n * log n)
+     * <p>
+     * https://leetcode.cn/problems/russian-doll-envelopes/solution/zui-chang-di-zeng-zi-xu-lie-kuo-zhan-dao-er-wei-er/
+     */
+    public int maxEnvelopesV2(int[][] envelopes) {
+        int n = envelopes.length;
+        // 按宽度升序排列，如果宽度一样，则按高度降序排列
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
+            }
+        });
+        // 对高度数组寻找 LIS
+        int[] height = new int[n];
+        for (int i = 0; i < n; i++) {
+            height[i] = envelopes[i][1];
+        }
+        return lengthOfLIS(height);
+    }
+
+    /**
+     * 返回 nums 中 LIS 的长度
+     */
+    public int lengthOfLIS(int[] nums) {
+        int piles = 0, n = nums.length;
+        int[] top = new int[n];
+        for (int i = 0; i < n; i++) {
+            // 要处理的扑克牌
+            int poker = nums[i];
+            int left = 0;
+            int right = piles;
+            // 二分查找插入位置
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] >= poker) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            // 没找到合适的牌堆，新建一堆
+            if (left == piles) {
+                piles++;
+            }
+            // 把这张牌放到牌堆顶
+            top[left] = poker;
+        }
+        // 牌堆数就是 LIS 长度
+        return piles;
+    }
+}
