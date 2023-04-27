8b890ea459aec0119f09527916bf46838f27285d
BeauteousJade <pby2196995023@163.com>
2023-04-27 21:57:48 +0800

    新增【109. 有序链表转换二叉搜索树】的解法

--- src/tree/SortedListToBSTDemo.java
+++ src/tree/SortedListToBSTDemo.java
@@ -42,4 +42,31 @@ public class SortedListToBSTDemo {
         root.right = sortedListToBST(list, (start + end) / 2 + 1, end);
         return root;
     }
+
+
+    public TreeNode sortedListToBSTV2(ListNode head) {
+        return buildTree(head, null);
+    }
+
+    public TreeNode buildTree(ListNode left, ListNode right) {
+        if (left == right) {
+            return null;
+        }
+        ListNode mid = getMedian(left, right);
+        TreeNode root = new TreeNode(mid.val);
+        root.left = buildTree(left, mid);
+        root.right = buildTree(mid.next, right);
+        return root;
+    }
+
+    public ListNode getMedian(ListNode left, ListNode right) {
+        ListNode fast = left;
+        ListNode slow = left;
+        while (fast != right && fast.next != right) {
+            fast = fast.next;
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
 }
