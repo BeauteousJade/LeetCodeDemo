1f649981ab552537d49cbedde57e72d9c9105532
BeauteousJade <pby2196995023@163.com>
2023-04-29 16:55:46 +0800

    增加【剑指 Offer 21. 调整数组顺序使奇数位于偶数前面】的写法

--- src/twoPointer/ExchangeDemo.java
+++ src/twoPointer/ExchangeDemo.java
@@ -1,5 +1,7 @@
 package twoPointer;
 
+import java.util.Arrays;
+
 /**
  * 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
  * <p>
@@ -8,9 +10,13 @@ package twoPointer;
 public class ExchangeDemo {
 
     public static void main(String[] args) {
-
+        ExchangeDemo demo = new ExchangeDemo();
+        System.out.println(Arrays.toString(demo.exchangeV2(new int[]{1, 2, 3, 4, 5, 6, 7})));
     }
 
+    /**
+     * 这个不保证相对顺序.时间复杂度是O(n)
+     */
     public int[] exchange(int[] array) {
         int fastIndex = 0;
         int slowIndex = 0;
@@ -19,6 +25,29 @@ public class ExchangeDemo {
                 int temp = array[slowIndex];
                 array[slowIndex] = array[fastIndex];
                 array[fastIndex] = temp;
+                slowIndex++;
+            }
+            fastIndex++;
+        }
+        return array;
+    }
+
+    /**
+     * 这个能保证相对顺序。时间复杂度是O(n * n)
+     */
+    public int[] exchangeV2(int[] array) {
+        int fastIndex = 0;
+        int slowIndex = 0;
+        while (fastIndex < array.length) {
+            if (array[fastIndex] % 2 == 1) {
+                int temp = array[fastIndex];
+                int moveIndex = fastIndex;
+                // 移动前面所有偶数
+                while (moveIndex > slowIndex) {
+                    array[moveIndex] = array[moveIndex - 1];
+                    moveIndex--;
+                }
+                array[slowIndex] = temp;
                 slowIndex++;
             }
             fastIndex++;
