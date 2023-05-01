aadb26790d5f2a95e99d14f41612d4e3dd39da4a
BeauteousJade <pby2196995023@163.com>
2023-05-01 22:31:31 +0800

    新增【74. 搜索二维矩阵】的解法

--- src/binarySearch/SearchMatrixDemo.java
+++ src/binarySearch/SearchMatrixDemo.java
@@ -29,4 +29,21 @@ public class SearchMatrixDemo {
         }
         return false;
     }
+
+    public boolean searchMatrixV2(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = m * n;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int value = matrix[mid / n][mid % n];
+            if (value >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left < m * n && matrix[left / n][left % n] == target;
+    }
 }
