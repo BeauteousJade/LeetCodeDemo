9f3183dff23aabc8720e46dcc3f8495fa3072fd5
BeauteousJade <pby2196995023@163.com>
2023-05-03 21:52:38 +0800

    修改【63. 不同路径 II】的解法

--- src/dp/UniquePathsWithObstaclesDemo.java
+++ src/dp/UniquePathsWithObstaclesDemo.java
@@ -38,4 +38,27 @@ public class UniquePathsWithObstaclesDemo {
         }
         return dp[dp.length - 1][dp[0].length - 1];
     }
+
+    /**
+     * 压缩空间
+     */
+    public int uniquePathsWithObstaclesV2(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[] f = new int[m];
+
+        f[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+
+        return f[m - 1];
+    }
 }
