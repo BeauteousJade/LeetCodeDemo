d699305b4e9a00e8f1b65779633cafebe89941f0
BeauteousJade <pby2196995023@163.com>
2023-05-04 21:04:05 +0800

    增加【162. 寻找峰值】的二分解法

--- src/other/FindPeakElementDemo.java
+++ src/other/FindPeakElementDemo.java
@@ -20,4 +20,17 @@ public class FindPeakElementDemo {
         }
         return idx;
     }
+
+    public int findPeakElementV2(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
+    }
 }
