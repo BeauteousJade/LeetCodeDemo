921bb5a03860c7b930c0a2b7705b12671eb24029
BeauteousJade <pby2196995023@163.com>
2023-05-07 21:20:48 +0800

    记录下次TODO

--- src/bfs/AlienOrderDemo.java
+++ src/bfs/AlienOrderDemo.java
@@ -7,6 +7,7 @@ import java.util.*;
  * <p>
  * https://leetcode.cn/problems/alien-dictionary/
  */
+// TODO 下次
 public class AlienOrderDemo {
 
     public static void main(String[] args) {
--- src/trie/MinimumLengthEncodingDemo.java
+++ src/trie/MinimumLengthEncodingDemo.java
@@ -1,5 +1,6 @@
 package trie;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
