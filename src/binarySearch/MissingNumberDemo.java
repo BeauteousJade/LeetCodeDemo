46b57722994149027908acfeb5436534196ff4f2
BeauteousJade <pby2196995023@163.com>
2023-05-08 21:49:24 +0800

    删除【剑指 Offer 53 - II. 0～n-1中缺失的数字】的错误代码

--- src/binarySearch/MissingNumberDemo.java
+++ src/binarySearch/MissingNumberDemo.java
@@ -11,26 +11,7 @@ public class MissingNumberDemo {
 
     }
 
-    /**
-     * 1.先看是否left等于right，如果等于就两个都变，结果优先选择left。
-     * 2.如果不能等于就一个变，最终结果选择变得那个。
-     */
     public int missingNumber(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            // 如果当前值等于 mid，表示丢失的数字在右侧。
-            if (nums[mid] == mid) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
-    }
-
-    public int missingNumberV2(int[] nums) {
         int left = 0;
         int right = nums.length;
         while (left < right) {
