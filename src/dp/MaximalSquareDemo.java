e0680e2fea70f19ab0e2796b9f37c79887ea66cc
BeauteousJade <pby2196995023@163.com>
2023-05-22 20:43:55 +0800

    简化【221. 最大正方形】的代码

--- src/dp/MaximalSquareDemo.java
+++ src/dp/MaximalSquareDemo.java
@@ -14,20 +14,11 @@ public class MaximalSquareDemo {
     }
 
     public int maximalSquare(char[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix[0].length];
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
         int res = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            dp[i][0] = matrix[i][0] == '1' ? 1 : 0;
-            res = Math.max(res, dp[i][0]);
-        }
-        for (int i = 0; i < matrix[0].length; i++) {
-            dp[0][i] = matrix[0][i] == '1' ? 1 : 0;
-            res = Math.max(res, dp[0][i]);
-        }
-
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[0].length; j++) {
-                if (matrix[i][j] == '1') {
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
                     dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1;
                 }
                 res = Math.max(res, dp[i][j]);
